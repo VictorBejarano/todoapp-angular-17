e9a59a3876707bb658f7cdbd2e2d36553a1fd7a1
Victor Bejarano <alejandrobjrn4@gmail.com>
2024-02-17 11:22:24 -0500

    5. Event Binding: click y doble click

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,24 +1,42 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css',
+  styleUrls: ['./labs.component.css']
 })
 export class LabsComponent {
-  welcome = 'Hola!';
-  tasks = ['Instalaer el angular CLI', 'Crear proyecto', 'Crear componentes'];
-  name = 'Victor';
-  age = 32;
+  welcome = 'Bienvenido a mi primera aplicación con Angular';
+  tasks = [
+    'Instalar Angular CLI',
+    'Crear proyecto',
+    'Crear componente',
+    'Crear servicio',
+  ];
+  name = 'Nicolas';
+  age = 18;
   disabled = true;
   img = 'https://w3schools.com/howto/img_avatar.png';
 
   person = {
-    name: 'Victor',
+    name: 'Nicolas',
     age: 18,
-    avatar: 'https://w3schools.com/howto/img_avatar.png',
-  };
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  }
+
+  clickHandler() {
+    alert('Hola')
+  }
+
+  changeHandler(event: Event) {
+    console.log(event);
+  }
+
+  keydownHandler(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
